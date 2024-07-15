e50cb1cc9a9e59e0f7e0fa44931bc591ce9e3dc1
Carolina Quiles <145223793+carolinaquiles@users.noreply.github.com>
2024-07-15 08:07:14 -0400

    updated codes
    added codes

--- src/app.js
+++ src/app.js
@@ -5,10 +5,17 @@ const articlesRouter = require("./articles/articles.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
+//const pinoHttp = require('pino-http')
+const logger = require("./config/logger");
+
 const app = express();
 
+//app.use(pinoHttp());
+app.use(logger);
 app.use(cors());
 app.use(express.json());
+
+// Some code omitted for brevity
 
 app.use("/articles", articlesRouter);
 
